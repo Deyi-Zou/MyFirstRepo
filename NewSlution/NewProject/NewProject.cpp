1b56a9611156eb85dec6916ddaff8d660456f404
Deyi Zou <Dzou1734@conestogac.on.ca>
2024-04-10 12:09:19 -0400

    make change the first time.

---
+++ NewSlution/NewProject/NewProject.cpp
@@ -0,0 +1,12 @@
+// NewProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+// make change the first time.
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
